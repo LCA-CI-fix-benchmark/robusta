71ae3bae5146d9b46e3e9ce74ca69170cb56a791
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-16 03:25:43 +0000

    waleko__gpt-4o_e0fd__id_205

--- src/robusta/utils/silence_utils.py
+++ src/robusta/utils/silence_utils.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel, SecretStr, validator
 from robusta.core.exceptions import AlertsManagerNotFound, NoAlertManagerUrlFound
 from robusta.core.model.base_params import ActionParams
 from robusta.integrations.prometheus.utils import AlertManagerDiscovery, ServiceDiscovery
-from src.robusta.integrations import openshift
+from ..integrations import openshift
 
 # ref to api https://github.com/prometheus/alertmanager/blob/main/api/v2/openapi.yaml
 
