ec97a94f948882fb6eb4d4aad27ac322728258c0
ubuntu <ubuntu@ip-10-0-1-31>
2025-01-16 02:57:22 +0000

    waleko__llama-3.2-3b_532e__id_205

--- src/robusta/utils/silence_utils.py
+++ src/robusta/utils/silence_utils.py
@@ -13,7 +13,89 @@ from robusta.core.model.base_params import ActionParams
 from robusta.integrations.prometheus.utils import AlertManagerDiscovery, ServiceDiscovery
 from src.robusta.integrations import openshift
 
-# ref to api https://github.com/prometheus/alertmanager/blob/main/api/v2/openapi.yaml
+class Matcher(BaseModel):
+    # https://github.com/prometheus/alertmanager/blob/main/api/v2/models/matcher.go
+    isEqual: bool = (
+        True  # support old version matchers with omitted isEqual https://github.com/prometheus/alertmanager/pull/2603
+    )
+    isRegex: bool
+    name: str
+    value: str
+class SilenceStatus(BaseModel):
+    # https://github.com/prometheus/alertmanager/blob/main/api/v2/models/silence_status.go
+    state: str
+class Silence(BaseModel):
+    # https://github.com/prometheus/alertmanager/blob/main/api/v2/models/silence.go
+    id: UUID
+    status: SilenceStatus
+    comment: str
+    createdBy: str
+    startsAt: datetime
+    endsAt: datetime
+    matchers: List[Matcher]
+class BaseSilenceParams(ActionParams):
+    """
+    :var alertmanager_url: Alternative Alert Manager url to send requests.
+    """
+    alertmanager_flavor: str = None  # type: ignore
+    alertmanager_url: Optional[str]
+    alertmanager_auth: Optional[SecretStr] = None
+    grafana_api_key: str = None  # type: ignore
+    @validator("alertmanager_url", allow_reuse=True)
+    def validate_alertmanager_url(cls, v):
+        if v and not v.startswith("http"):  # if the user configured url without http(s)
+            v = f"http://{v}"
+            logging.info(f"Adding protocol to alertmanager_url: {v}")
+        if v.endswith("/"):
+            return v[:-1]
+        return v
+    @validator("alertmanager_auth", allow_reuse=True, always=True)
+    def auto_openshift_token(cls, v: Optional[SecretStr]):
+        # If openshift is enabled, and the user didn't configure alertmanager_auth, we will try to load the token from the service account
+        if v is not None:
+            return v
+        openshift_token = openshift.load_token()
+        if openshift_token is not None:
+            logging.info(f"Using openshift token from {openshift.TOKEN_LOCATION} for alertmanager auth")
+            return SecretStr(f"Bearer {openshift_token}")
+    class DeleteSilenceParams(BaseSilenceParams):
+        """
+        :var id: uuid of the silence.
+        """
+
+        id: str
+    class AddSilenceParams(BaseSilenceParams):
+        """
+        :var id: uuid of the silence. use for update, empty on create.
+        :var comment: text comment of the silence.
+        :var createdBy: author of the silence.
+        :var startsAt: date.
+        :var endsAt: date.
+        :var matchers: List of matchers to filter the silence effect.
+        """
+
+        id: Optional[str]
+        comment: str
+        createdBy: str
+        startsAt: datetime
+        endsAt: datetime
+        matchers: List[Matcher]
+def get_alertmanager_silences_connection(params: BaseSilenceParams):
+    alertmanager_url = get_alertmanager_url(params)
+
+    if not alertmanager_url:
+        raise NoAlertManagerUrlFound("AlertManager url could not be found. Add 'alertmanager_url' under global_config")
+    try:
+        response = requests.get(
+            f"{alertmanager_url}{get_alertmanager_url_path(SilenceOperation.LIST, params)}",
+            headers=gen_alertmanager_headers(params),
+        )
+        response.raise_for_status()
+
+        except Exception as e:
+            raise AlertsManagerNotFound(
+                f"Could not connect to the alert manager [{alertmanager_url}] \nCaused by {e.__class__.__name__}: {e})"
+            ) from e
 
 
 class Matcher(BaseModel):
